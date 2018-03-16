31f9acf8d8ec43659a0f12b226b3724699fa45ef
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-03-16 17:29:53 +0100

    Initial
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

---
+++ hello_openshift.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	jaeger "github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	sp, _ := opentracing.StartSpanFromContext(r.Context(), "request")
+	defer sp.Finish()
+
+	response := os.Getenv("RESPONSE")
+	if len(response) == 0 {
+		response = "Hello OpenShift!"
+	}
+
+	fmt.Fprintln(w, response)
+	fmt.Println("Servicing request with a span.")
+}
+
+func listenAndServe(port string) {
+	fmt.Printf("serving on %s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
+
+func main() {
+	config := &jaegercfg.Configuration{
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+
+	closer, err := config.InitGlobalTracer("hello-openshift", jaegercfg.Logger(jaeger.StdLogger))
+	if err != nil {
+		log.Printf("Could not initialize jaeger tracer: %s", err.Error())
+		return
+	}
+	defer closer.Close()
+
+	log.Println("Jaeger tracer initialized")
+
+	http.HandleFunc("/", helloHandler)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	listenAndServe(port)
+}
